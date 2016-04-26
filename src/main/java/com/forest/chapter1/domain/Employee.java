75b02343b3f3d0d56c42af26d78d27a950ffef98
forestaiste <forestaiste@hotmail.com>
2016-04-26 13:21:38 +0800

    Initial commit

---
+++ src/main/java/com/forest/chapter1/domain/Employee.java
@@ -0,0 +1,50 @@
+package com.forest.chapter1.domain;
+
+
+/**
+ * Created by A21962 on 4/22/2016.
+ */
+public class Employee{
+    private String firstName;
+    private String lastName;
+    private Address homeAddress;
+
+    public Employee() {
+    }
+
+    public Employee(String firstName, String lastName, Address homeAddress) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.homeAddress = homeAddress;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName
+                + "\n" + homeAddress;
+    }
+}
